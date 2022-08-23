f1863ff41d04b78be446c06e483ee453df588d13
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-08-23 13:48:11 +0200

    Add files via upload

---
+++ item5/Sözlük.java
@@ -0,0 +1,13 @@
+package com.effectivejava.tutorial.effectivejava.item5;
+
+public class Sözlük {
+    private String dil;
+
+    public Sözlük(String dil) {
+        this.dil = dil;
+    }
+
+    public String toString() {
+        return dil;
+    }
+}
