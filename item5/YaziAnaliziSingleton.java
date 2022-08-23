c3aaf5c1b64721107a430ee6b5448ef602b8fde5
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-08-23 13:48:38 +0200

    Add files via upload

---
+++ item5/YaziAnaliziSingleton.java
@@ -0,0 +1,15 @@
+package com.effectivejava.tutorial.effectivejava.item5;
+
+public class YaziAnaliziSingleton {
+
+    private static final Sözlük türkce = new Sözlük("Türkce");
+
+    public static YaziAnaliziSingleton INSTANCE = new YaziAnaliziSingleton();
+    private YaziAnaliziSingleton(){}   // Tek 1 instance olup onun üzerinden calismasi gerekiyor
+
+    public static boolean isValid() {
+        return true;
+    }
+
+
+}
