31c88ecb36d7ee729431fc92ebebf460e3b156a1
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-08-23 13:49:28 +0200

    Add files via upload

---
+++ item5/YaziAnaliziUtility.java
@@ -0,0 +1,12 @@
+package com.effectivejava.tutorial.effectivejava.item5;
+
+public class YaziAnaliziUtility {
+
+    private static final Sözlük sözlük = new Sözlük("Türkce");
+
+    private YaziAnaliziUtility() {}
+
+    public static boolean isValid() {
+        return true;
+    }
+}
